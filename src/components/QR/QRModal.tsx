d7140541e91b54a1c36d6a3b7156c12a0268fc35
Compdog Inc. <66779418+Compdog-inc@users.noreply.github.com>
2025-03-10 23:48:03 -0400

    Add QRCode copy button

--- src/components/QR/QRModal.tsx
+++ src/components/QR/QRModal.tsx
@@ -1,6 +1,6 @@
 import { Copy, QrCode } from 'lucide-react';
-import { QRCodeSVG } from 'qrcode.react';
-import { useMemo } from 'react';
+import { QRCodeSVG, QRCodeCanvas } from 'qrcode.react';
+import { useCallback, useMemo, useRef } from 'react';
 import { getFieldValue, useQRScoutState } from '../../store/store';
 import { Button } from '../ui/button';
 import {
@@ -17,6 +17,8 @@ export interface QRModalProps {
 }
 
 export function QRModal(props: QRModalProps) {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const blobRef = useRef<Blob | null>(null);
   const fieldValues = useQRScoutState(state => state.fieldValues);
   const formData = useQRScoutState(state => state.formData);
   const title = `${getFieldValue('robot')} - M${getFieldValue(
@@ -27,35 +29,69 @@ export function QRModal(props: QRModalProps) {
     () => fieldValues.map(f => f.value).join(formData.delimiter),
     [fieldValues],
   );
+
+  const canvasBlob = useCallback(() => {
+    const node = canvasRef.current;
+    if (node == null) {
+      return;
+    }
+
+    // For canvas, we just extract the image data and send that directly.
+    node.toBlob(blob => {
+      blobRef.current = blob;
+    });
+  }, [canvasRef]);
+
   //Two seperate values are required- qrCodePreview is what is shown to the user beneath the QR code, qrCodeData is the actual data.
 
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button disabled={props.disabled}>
-          <QrCode className="size-5" />
-          Commit
-        </Button>
-      </DialogTrigger>
-      <DialogContent className="h-[95%]">
-        <DialogTitle className="text-3xl text-primary text-center font-rhr-ns tracking-wider ">
-          {title}
-        </DialogTitle>
-        <div className="flex flex-col items-center gap-6 overflow-y-scroll">
-          <div className="bg-white p-4 rounded-md">
-            <QRCodeSVG className="m-2 mt-4" size={256} value={qrCodeData} />
-          </div>
-          <PreviewText data={qrCodeData} />
-        </div>
-        <DialogFooter>
-          <Button
-            variant="ghost"
-            onClick={() => navigator.clipboard.writeText(qrCodeData)}
-          >
-            <Copy className="size-4" /> Copy Data
+    <><QRCodeCanvas ref={canvasRef} size={256} value={qrCodeData} marginSize={4} style={{ display: 'none' }} />
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button disabled={props.disabled} onClick={canvasBlob}>
+            <QrCode className="size-5" />
+            Commit
           </Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+        </DialogTrigger>
+        <DialogContent className="h-[95%]">
+          <DialogTitle className="text-3xl text-primary text-center font-rhr-ns tracking-wider ">
+            {title}
+          </DialogTitle>
+          <div className="flex flex-col items-center gap-6 overflow-y-scroll">
+            <div className="bg-white p-4 rounded-md">
+              <QRCodeSVG className="m-2 mt-4" size={256} value={qrCodeData} />
+            </div>
+            <PreviewText data={qrCodeData} />
+          </div>
+          <DialogFooter>
+            <Button
+              variant="ghost"
+              onClick={() => {
+                const blob = blobRef.current;
+
+                if (blob == null) {
+                  alert("Error copying image");
+                  return;
+                }
+
+                const item = new ClipboardItem({ "image/png": blob });
+                try {
+                  navigator.clipboard.write([item]);
+                } catch {
+                  alert("Error copying image");
+                }
+              }}
+            >
+              <Copy className="size-4" /> Copy Image
+            </Button>
+            <Button
+              variant="ghost"
+              onClick={() => navigator.clipboard.writeText(qrCodeData)}
+            >
+              <Copy className="size-4" /> Copy Data
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog></>
   );
 }
